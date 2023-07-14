a462a8ead1597df395a45301f15e4620a19a37e1
Dmitry Dmitry <7353687@gmail.com>
2023-07-13 23:14:47 -0400

    added dates and cityes list

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,33 +1,43 @@
-// var requestUrl = ' https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=London,uk&appid=0d050478da7af8cff791a18ab4834659'
 
 var responseText = document.getElementById('response-text');
 var btn = document.querySelector(".btn");
 var city = document.querySelector("#cityInput");
+var oldCity = $('#cityName');
 var x = localStorage.getItem("story");
-var story = x.split(",");
-console.log("🚀 ~ file: script.js:8 ~ story:", story, typeof(story));
+if (x === null) { } else {
+  var story = x.split(",");
+  console.log("🚀 ~ file: script.js:8 ~ story:", story, typeof (story));
 
-for (var i = 0; i < story.length; i++){
+  for (var i = 0; i < story.length; i++) {
 
-  console.log("🚀 ~ file: script.js:46 ~ i:", i)
-  
-  var butt = $('button');
-  var buttons = $('#buttons');
-  butt.addClass('list-group-item list-group-item-action');
-  butt.attr('type', 'button');
-  console.log(story[i]);
-  butt.text(story[i]);
-  buttons.append(butt);
+    console.log("🚀 ~ file: script.js:46 ~ i:", i)
+
+    var butt = $('<button>>');
+    var buttons = $('#buttons');
+    butt.addClass('list-group-item list-group-item-action');
+    // butt.attr('type', 'button');
+    butt.attr('id', 'cityName');
+    buttons.append(butt);
+    console.log(story[i]);
+    butt.text(story[i]);
+  }
 };
-
-
 
 btn.addEventListener('click', function () {
   console.log(city.value + typeof (city));
   var cityString = JSON.stringify(city.value);
-  console.log("🚀 ~ file: script.js:13 ~ cityString:", cityString, typeof(cityString))
-  if (story === null) {
-    story = [cityString]
+  console.log("🚀 ~ file: script.js:13 ~ cityString:", cityString, typeof (cityString))
+  console.log("🚀 ~ file: script.js:32 ~ story:", story)
+  if (x === null) {
+    story = [cityString];
+    var butt = $('<button>');
+    var buttons = $('#buttons');
+    butt.addClass('list-group-item list-group-item-action');
+    butt.attr('type', 'button');
+    butt.attr('id', 'cityName');
+    console.log(story);
+    buttons.append(butt);
+    butt.text(story);
   } else {
     story.push(cityString);
 
@@ -45,10 +55,13 @@ btn.addEventListener('click', function () {
 
       console.log(data);
       var nameval = data.name;
+      var unixtime = data.dt;
+      var time = dayjs.unix(unixtime).format('MMM D, YYYY');
       var descrip = data.weather[0].description;
       var humi = data.main.humidity;
       var tempature = data.main.temp;
       var wndspd = data.wind.speed;
+      today.innerHTML = `Today: <span>${time}<span>`
       cityName.innerHTML = `City: <span>${nameval}<span>`
       temp.innerHTML = `Temperature: <span>${tempature} F</span>`
       clouds.innerHTML = `Clouds: <span>${descrip}<span>`
@@ -56,7 +69,6 @@ btn.addEventListener('click', function () {
       wind.innerHTML = `Wind Speed: <span>${wndspd} MPH<span>`
     });
 
-   
   var requestUrlForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city.value + '&units=imperial&appid=0d050478da7af8cff791a18ab4834659'
 
   fetch(requestUrlForecast)
@@ -67,27 +79,37 @@ btn.addEventListener('click', function () {
     })
     .then(function (data) {
       console.log(data);
-     
+
+      var unixtime = data.list[6].dt;
+      var time1 = dayjs.unix(unixtime).format('MMM D, YYYY');
       var descrip1 = data.list[6].weather[0].description;
       var tempature1 = data.list[6].main.temp;
       var wndspd1 = data.list[6].wind.speed;
       var humi1 = data.list[6].main.humidity;
 
+      var unixtime = data.list[14].dt;
+      var time2 = dayjs.unix(unixtime).format('MMM D, YYYY');
       var descrip2 = data.list[14].weather[0].description;
       var tempature2 = data.list[14].main.temp;
       var wndspd2 = data.list[14].wind.speed;
       var humi2 = data.list[14].main.humidity;
 
+      var unixtime = data.list[22].dt;
+      var time3 = dayjs.unix(unixtime).format('MMM D, YYYY');
       var descrip3 = data.list[22].weather[0].description;
       var tempature3 = data.list[22].main.temp;
       var wndspd3 = data.list[22].wind.speed;
       var humi3 = data.list[22].main.humidity;
 
+      var unixtime = data.list[30].dt;
+      var time4 = dayjs.unix(unixtime).format('MMM D, YYYY');
       var descrip4 = data.list[30].weather[0].description;
       var tempature4 = data.list[30].main.temp;
       var wndspd4 = data.list[30].wind.speed;
       var humi4 = data.list[30].main.humidity;
 
+      var unixtime = data.list[38].dt;
+      var time5 = dayjs.unix(unixtime).format('MMM D, YYYY');
       var descrip5 = data.list[38].weather[0].description;
       var tempature5 = data.list[38].main.temp;
       var wndspd5 = data.list[38].wind.speed;
@@ -97,29 +119,38 @@ btn.addEventListener('click', function () {
       clouds1.innerHTML = `Clouds: <span>${descrip1}<span>`
       wind1.innerHTML = `Wind Speed: <span>${wndspd1} MPH<span>`
       hum1.innerHTML = `Humidity: <span>${humi1}%<span>`
+      today1.innerHTML = `Today: <span>${time1}<span>`
 
       temp2.innerHTML = `Temperature: <span>${tempature2} F</span>`
       clouds2.innerHTML = `Clouds: <span>${descrip2}<span>`
       wind2.innerHTML = `Wind Speed: <span>${wndspd2} MPH<span>`
       hum2.innerHTML = `Humidity: <span>${humi2}%<span>`
+      today2.innerHTML = `Today: <span>${time2}<span>`
 
       temp3.innerHTML = `Temperature: <span>${tempature3} F</span>`
       clouds3.innerHTML = `Clouds: <span>${descrip3}<span>`
       wind3.innerHTML = `Wind Speed: <span>${wndspd3} MPH<span>`
       hum3.innerHTML = `Humidity: <span>${humi3}%<span>`
+      today3.innerHTML = `Today: <span>${time3}<span>`
 
       temp4.innerHTML = `Temperature: <span>${tempature4} F</span>`
       clouds4.innerHTML = `Clouds: <span>${descrip4}<span>`
       wind4.innerHTML = `Wind Speed: <span>${wndspd4} MPH<span>`
       hum4.innerHTML = `Humidity: <span>${humi4}%<span>`
+      today4.innerHTML = `Today: <span>${time4}<span>`
 
       temp5.innerHTML = `Temperature: <span>${tempature5} F</span>`
       clouds5.innerHTML = `Clouds: <span>${descrip5}<span>`
       wind5.innerHTML = `Wind Speed: <span>${wndspd5} MPH<span>`
       hum5.innerHTML = `Humidity: <span>${humi5}%<span>`
+      today5.innerHTML = `Today: <span>${time5}<span>`
 
-
-      // }
     });
+
+});
+
+oldCity.on('click', function (event) {
+  var y = event.target;
+  console.log("🚀 ~ file: script.js:142 ~ oldCity.addEventListener ~ y:", y)
 
 })
